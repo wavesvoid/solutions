8a47656a1299cdc404900abb4039385ede5b906b
fiberether <ethereum51@gmail.com>
2023-09-08 01:05:38 +0300

    llearning:ess:task_9_4:working_solution_no_fmt

---
+++ linkedin_learning/RustEssentialTraining/task_9_4/src/main.rs
@@ -0,0 +1,33 @@
+//! Check if a file contains a specific name
+
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+
+fn main() {
+    let mut args = env::args().skip(1).take(2);
+    let (filename, search_name): (String, String);
+
+    match args.len() {
+        2 => (filename, search_name) = (args.next().unwrap(), args.next().unwrap()),
+        _ => {
+            println!("Provide exactly two arguments: {filename} {name}");
+            return;
+        }
+    }
+
+    let file = File::open(&filename)
+        .expect(&format!("Cannot open {}", &filename));
+    let mut linesbuf = BufReader::new(file).lines();
+
+    while let Some(Ok(line)) = linesbuf.next() {
+        if line.eq(&search_name) {
+            println!("This file contains given item ({}) in the list",
+                search_name);
+            return;
+        }
+    }
+
+    println!("This item ({}) not found in list", search_name);
+}
