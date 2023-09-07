09266c3c73d4096d4e99b9782c6c64b240e9eff4
fiberether <ethereum51@gmail.com>
2023-09-08 01:54:04 +0300

    llearning:ess:task_9_4:code_prettify

--- linkedin_learning/RustEssentialTraining/task_9_4/src/main.rs
+++ linkedin_learning/RustEssentialTraining/task_9_4/src/main.rs
@@ -7,24 +7,21 @@ use std::io::{BufReader, BufRead};
 
 fn main() {
     let mut args = env::args().skip(1).take(2);
-    let (filename, search_name): (String, String);
 
-    match args.len() {
-        2 => (filename, search_name) = (args.next().unwrap(), args.next().unwrap()),
-        _ => {
-            println!("Provide exactly two arguments: {filename} {name}");
-            return;
-        }
+    if args.len() != 2 {
+        println!("Provide exactly two arguments: {filename} {name}");
+        return;
     }
-
+    
+    let (filename, search_name) = (args.next().unwrap(), args.next().unwrap());
     let file = File::open(&filename)
         .expect(&format!("Cannot open {}", &filename));
     let mut linesbuf = BufReader::new(file).lines();
 
     while let Some(Ok(line)) = linesbuf.next() {
         if line.eq(&search_name) {
-            println!("This file contains given item ({}) in the list",
-                search_name);
+            println!(
+                "This file contains given item ({}) in the list", search_name);
             return;
         }
     }
