a580295164e5e2562632d6570c5ebec1bbd4414b
fiberether <ethereum51@gmail.com>
2023-09-07 17:06:55 +0300

    llearning:ess:task_8_6_correction_improvements

--- linkedin_learning/RustEssentialTraining/task_8_6/src/main.rs
+++ linkedin_learning/RustEssentialTraining/task_8_6/src/main.rs
@@ -5,21 +5,23 @@ use rand::prelude::*;
 fn main() {
     let mut input_buffer = String::with_capacity(10);
     let secret_number = thread_rng().gen_range(1..=100);
+    let mut guess: i32;
 
     loop {
         println!("Please enter your guess:");
         io::stdin().read_line(&mut input_buffer);
         
-        let guess: i32 = input_buffer.trim().parse().map_err(|e| {
-            println!("Cannot correctly read the number. Wrong input!");
-            e
-        }).unwrap();
-        match secret_number.cmp(&guess) {
-            std::cmp::Ordering::Less => println!("Too high!"),
-            std::cmp::Ordering::Greater => println!("Too low!"),
-            _ => {
-                println!("Success !! {}", guess);
-                break;
+        match input_buffer.trim().parse() {
+            Err(_) => {
+                println!("Cannot correctly read the number. Wrong input!");
+            }
+            Ok(guess) => match secret_number.cmp(&guess) {
+                std::cmp::Ordering::Less => println!("Too high!"),
+                std::cmp::Ordering::Greater => println!("Too low!"),
+                _ => {
+                    println!("Success !! {}", guess);
+                    break;
+                }
             }
         }
         input_buffer.clear();
