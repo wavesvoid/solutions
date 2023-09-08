609e061626f72f1de2f83a434d2871336f9fb420
fiberether <ethereum51@gmail.com>
2023-09-08 23:40:41 +0300

    llearning:ess:task_12_7:bonus_challenge

--- linkedin_learning/RustEssentialTraining/task_12_7/src/main.rs
+++ linkedin_learning/RustEssentialTraining/task_12_7/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq)]
 struct Satellite {
     name: String,
     velocity: f64 // miles per second
@@ -5,13 +6,39 @@ struct Satellite {
 
 /* YOUR CODE GOES HERE */
 use std::fmt::{Display, Error, Formatter};
+
+const GRAVITATIONAL_ACC: f64 = 9.81;
+
+trait Altitude {
+    fn altitude(&self) -> f64;
+}
+
+impl Altitude for Satellite {
+    fn altitude(&self) -> f64 {
+        GRAVITATIONAL_ACC * (self.velocity / GRAVITATIONAL_ACC).powf(2.0) / 2.0
+    }
+}
+
 impl Display for Satellite {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        write!(f, "Satellite {{\n\tname: {},\n\tvelocity: {}\n}}", 
+        write!(f, 
+            "Satellite {{\n\tname: {},\n\tvelocity: {},\n\taltitude: {},\n}}", 
             self.name,
-            self.velocity)
+            self.velocity,
+            self.altitude())
     }
+}
 
+impl PartialOrd for Satellite {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let alt_gt = self.altitude() > other.altitude();
+        let vel_lt = self.velocity < other.velocity;
+
+        Some(match alt_gt && vel_lt {
+            true => std::cmp::Ordering::Greater,
+            false => std::cmp::Ordering::Less,
+        })
+    }
 }
 
 
@@ -20,5 +47,17 @@ fn main() {
         name: String::from("Hubble Telescope"),
         velocity: 4.72
     };
+    let mubble = Satellite {
+        name: String::from("Mubble Tetrascope"),
+        velocity: 32.72
+    };
+
     println!("hubble is {}", hubble);
+    println!("mubble is {}", mubble);
+
+    if hubble > mubble {
+        println!("hubble is greater than mubble");
+    } else if hubble < mubble {
+        println!("mubble is greater than hubble");
+    }
 }
